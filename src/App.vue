d9c4a1833fd83edd5750c4e979592c3f19d0d20d
Lea23VC <leandro.villalobos.dx@gmail.com>
2023-09-23 23:42:37 -0300

    first commit!

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="resource in storedResources"
+      :key="resource.id"
+      :resource="resource"
+    >
+    </learning-resource>
+  </ul>
+</template>
+
+<script>
+import ResourceItem from '@/components/learning-resources/ResourceItem';
+
+export default {
+  components: {
+    'learning-resource': ResourceItem,
+  },
+
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official guide to Vue.js.',
+          link: 'https://vuejs.org/',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Where you search for things.',
+          link: 'https://google.com/',
+        },
+        {
+          id: 'youtube',
+          title: 'YouTube',
+          description: 'Where you watch things.',
+          link: 'https://youtube.com/',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/learning-resources/ResourceItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{ resource.title }}</h3>
+        <p>{{ resource.description }}</p>
+
+        <button>Delete</button>
+      </header>
+    </div>
+    <p></p>
+    <nav>
+      <a :href="resource.link">Visit</a>
+    </nav>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    resource: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
